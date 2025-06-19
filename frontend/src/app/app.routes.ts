aa28bb23362e6df623ee2d9413234937651586cf
Hunter Thiel <hbomb1523@gmail.com>
2025-06-18 20:12:14 -0500

    Updated all of the changes to the frontend files.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {Register} from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/register', pathMatch: 'full'}, // Redirect to register by default
+    {path: 'register', component: Register} // Add this route
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+
+export class AppRoutingModule {}
